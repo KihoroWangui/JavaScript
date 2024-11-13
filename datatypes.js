734e37ce52499fa829816aafbdde8d3f528ff916
KihoroWangui <94446783+KihoroWangui@users.noreply.github.com>
2024-11-13 13:35:48 +0300

    second commit

--- datatypes.js
+++ datatypes.js
@@ -1,2 +1,17 @@
-let n1 = 3;
-console.log(n1);
+//infiinity
+let n2 = 1;
+console.log(n2);
+let s = "Hello There";
+console.log(s);
+//EAS6 primitive datatype used as an identifier for onject properties
+let s1 = Symbol("hello");
+let s2 = Symbol("hello");
+console.log(s1 == s2);
+//BigInt builtin object to represent primitive number higher than 2^53
+let b = BigInt("0b1010101001010101001111111111111111");
+Console.log(b);
+//object syntax used to store collections of data
+let o = {};
+//array
+let a = [1, 2, 3, 4, 5];
+console.log(a);
